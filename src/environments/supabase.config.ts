eff65b1f08940c645013e4265003814a0cc16052
Futurannet Design <futurannetdesign@hotmail.com>
2025-03-05 18:04:08 -0300

    criação do supabase tabelas

---
+++ src/environments/supabase.config.ts
@@ -0,0 +1,5 @@
+// src/environments/supabase.config.ts
+export const supabaseConfig = {
+  url: 'https://ygqnwpcgjrpmsgqnwlot.supabase.co', // Substitua pela sua URL do projeto
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlncW53cGNnanJwbXNncW53bG90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNzU4ODgsImV4cCI6MjA1Njc1MTg4OH0.Ek7iESGrttpFScs7krfxiGpc57PoVl_9xANAOaEu2u4', // Substitua pela sua chave anon
+};
